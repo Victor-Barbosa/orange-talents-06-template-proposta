4c83513b21325a2d604532e0cea99b34cc47d246
victor-barbosa <victorbarbosadev@gmail.com>
2021-07-23 15:43:10 -0300

    commit inicial

---
+++ src/main/java/br/com/zupacademy/victor/orangetalents06templateproposta/OrangeTalents06TemplatePropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.victor.orangetalents06templateproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents06TemplatePropostaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTalents06TemplatePropostaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zupacademy/victor/orangetalents06templateproposta/OrangeTalents06TemplatePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.victor.orangetalents06templateproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTalents06TemplatePropostaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
